d25bf1bb563c7a9aaee975a1fb1771a600b28699
Nat! <nat@mulle-kybernetik.com>
2024-11-19 15:44:42 +0100

    update CI prerelease files

--- .mulle/etc/env/environment-host-ci-prerelease.sh
+++ .mulle/etc/env/environment-host-ci-prerelease.sh
@@ -35,6 +35,60 @@ export MULLE__OBJC__RUNTIME_BRANCH="prerelease"
 
 
 #
+# mulle-core nodetype: ${MULLE__CORE_NODETYPE:-tar}
+#
+export MULLE__CORE_NODETYPE="git"
+
+
+#
+# mulle-core url: ${MULLE__CORE_URL:-https://github.com/mulle-core/mulle-core/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__CORE_URL="https://github.com/mulle-core/mulle-core.git"
+
+
+#
+# mulle-core branch: ${MULLE__CORE_BRANCH}
+#
+export MULLE__CORE_BRANCH="prerelease"
+
+
+#
+# mulle-atinit nodetype: ${MULLE__ATINIT_NODETYPE:-tar}
+#
+export MULLE__ATINIT_NODETYPE="git"
+
+
+#
+# mulle-atinit url: ${MULLE__ATINIT_URL:-https://github.com/mulle-core/mulle-atinit/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__ATINIT_URL="https://github.com/mulle-core/mulle-atinit.git"
+
+
+#
+# mulle-atinit branch: ${MULLE__ATINIT_BRANCH}
+#
+export MULLE__ATINIT_BRANCH="prerelease"
+
+
+#
+# mulle-atexit nodetype: ${MULLE__ATEXIT_NODETYPE:-tar}
+#
+export MULLE__ATEXIT_NODETYPE="git"
+
+
+#
+# mulle-atexit url: ${MULLE__ATEXIT_URL:-https://github.com/mulle-core/mulle-atexit/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__ATEXIT_URL="https://github.com/mulle-core/mulle-atexit.git"
+
+
+#
+# mulle-atexit branch: ${MULLE__ATEXIT_BRANCH}
+#
+export MULLE__ATEXIT_BRANCH="prerelease"
+
+
+#
 # mulle-objc-debug nodetype: ${MULLE__OBJC__DEBUG_NODETYPE:-tar}
 #
 export MULLE__OBJC__DEBUG_NODETYPE="git"
@@ -86,41 +140,5 @@ export MULLE__OBJC__LIST_URL="https://github.com/mulle-objc/mulle-objc-list.git"
 # mulle-objc-list branch: ${MULLE__OBJC__LIST_BRANCH}
 #
 export MULLE__OBJC__LIST_BRANCH="prerelease"
-
-
-#
-# mulle-atinit nodetype: ${MULLE__ATINIT_NODETYPE:-tar}
-#
-export MULLE__ATINIT_NODETYPE="git"
-
-
-#
-# mulle-atinit url: ${MULLE__ATINIT_URL:-https://github.com/mulle-core/mulle-atinit/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ATINIT_URL="https://github.com/mulle-core/mulle-atinit.git"
-
-
-#
-# mulle-atinit branch: ${MULLE__ATINIT_BRANCH}
-#
-export MULLE__ATINIT_BRANCH="prerelease"
-
-
-#
-# mulle-atexit nodetype: ${MULLE__ATEXIT_NODETYPE:-tar}
-#
-export MULLE__ATEXIT_NODETYPE="git"
-
-
-#
-# mulle-atexit url: ${MULLE__ATEXIT_URL:-https://github.com/mulle-core/mulle-atexit/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ATEXIT_URL="https://github.com/mulle-core/mulle-atexit.git"
-
-
-#
-# mulle-atexit branch: ${MULLE__ATEXIT_BRANCH}
-#
-export MULLE__ATEXIT_BRANCH="prerelease"
 
 
